c1998cbbf0dc85a9c752135721aaaafccdcbed8e
evank28 <evankanter28@gmail.com>
2019-03-26 12:37:56 -0400

    v.20190326-v - Fixed the issue of the Robot.class dependency - Fixed and updated Assignement in Unit 4

--- Unit 4/EvanKanter_A42_mySavings.java
+++ Unit 4/EvanKanter_A42_mySavings.java
@@ -7,7 +7,7 @@ public class EvanKanter_A42_mySavings
         System.out.println("Please initialize the piggy bank. Enter the starting balance: ") ;
         Scanner initial = new Scanner (System.in) ;
         piggyBank myBank = new piggyBank(initial.nextDouble()) ;
-        initial.close();
+        //initial.close();
         myBank.initalize();
     }
 }
@@ -29,7 +29,7 @@ class piggyBank
     {
         System.out.println(" Please select one of the following options: \n 1. Show total in bank \n 2. Add a penny. \n 3. Add a nickel. \n 4. Add a dime. \n 5. Add a quarter. \n 6. Take money out of the bank. \n    Enter 0 to quit. \n") ;
         Scanner input = new Scanner (System.in) ;
-       /* while (true)*/ this.select(input) ;
+        while (true) this.select(input) ;   //Loops through waiting for the user to exit the program.
     }
     public void select (Scanner input)      //Uses a switch to make decisions 
     {
@@ -43,6 +43,7 @@ class piggyBank
 
             // The following input cases correspond to the the main menu UI that is printed in piggyBank.initialize()
             case 0:
+                System.out.println("Goodbye!") ;        //Prints an exit message and then exits the program.
                 System.exit(0) ;
 
             case 1:
@@ -74,37 +75,41 @@ class piggyBank
 
     public void printBalance()   // prints the balance in the bank
     {
-        System.out.println(balance) ;
+        System.out.println("Your current balance is: $"+ String.format("%.2f", balance) );
     }
 
     public void addPenny()       // adds $0.01 to the bank balance
     {
         balance+=0.01 ;
+        System.out.println("Congratulations - you've saved an extra penny!") ;
     }
 
     public void addNickel()      // adds $0.05 to the bank balance
     {
         balance+=0.05 ;
+        System.out.println("Congratulations - you've saved an extra Nickel!") ;
     }
 
     public void addDime()        // adds $0.10 to the bank balance
     {
-        balance+=0.05 ;
+        balance+=0.10 ;
+        System.out.println("Congratulations - you've saved an extra Dime!") ;
     }
 
     public void addQuarter()     // adds $0.25 to the bank balance
     {
         balance+=0.25 ;
+        System.out.println("Congratulations - you've saved an extra Quarter!") ;
     }
 
     public void withdraw(Scanner input)              //withdraws a user-specified amount from the bank or yelds an error if there are less funds than the user wishes to withdraw
     {
-        System.out.println ("How much would you like to Withdraw?") ;
+        System.out.print ("How much would you like to withdraw? \n$") ;
         double  withdrawal = input.nextDouble() ;      // assigns the next double in the stream to the withdraw variable
         if (balance - withdrawal >= 0.0)                // checks if the specified amount of funds is in the bank
         {
             balance-= withdrawal ;                      //subtracts the specified amoutn from the balance and confirms to the user
-            System.out.println ("You have succesfully withdrawn $"+withdrawal+" .\n") ;
+            System.out.println ("You have succesfully withdrawn $"+String.format("%.2f", withdrawal)+".\n") ;
         }
         else 
         {
