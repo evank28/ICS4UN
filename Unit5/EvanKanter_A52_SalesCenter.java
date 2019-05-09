4e5770bfb11b936cfe32f2d73fd800a9cbe95515
evank28 <evankanter28@gmail.com>
2019-05-09 16:42:51 -0400

    v.20190509-3 - Added commenting to both A52 and A53 - Fixed minor bugs in A52 and A53, including an IndexOutOfBounds error

--- Unit5/EvanKanter_A52_SalesCenter.java
+++ Unit5/EvanKanter_A52_SalesCenter.java
@@ -3,31 +3,34 @@ import java.util.Scanner;
 public class EvanKanter_A52_SalesCenter
 {
  public static void main (String[] args){
+    //initalize a database of employees stored as an Array
     Employee[] employeeDB = {new Manager("Diego", "Martin", 55000.00 ), new Associate("Kylie", "Walter", 18.50), new Associate("Michael", "Rose", 16.75)};
-    while (true){
+    
+    
+    while (true){       //begins looping, allowing the user to continously navigate through the menu until they select (Q)uit
         Scanner stdin = new Scanner (System.in);
-    System.out.print("Select one of the options: \n(E)mployee Information\n(P)ay\n(Q)uit\nEnter your choice: ");
-    String choice1 = stdin.next().toLowerCase();
-    if (choice1.equals("q")){
-        System.exit(1);
-    }
-    System.out.print("Enter Employee Number (1, 2, or 3): ");
-    Employee selection = employeeDB[stdin.nextInt()-1];
-    if (choice1.equals("p")){
-        try{ 
-            if (selection instanceof Associate){
-                System.out.print("Enter number of hours worked: ");
-                System.out.println(selection +"\n" + String.format("$%.2f", selection.pay(stdin.nextInt())));
+        System.out.print("\nSelect one of the options: \n(E)mployee Information\n(P)ay\n(Q)uit\nEnter your choice: ");             // prints the main menu
+        String choice1 = stdin.next().toLowerCase();            //reads the user's selection, converting it to lower case
+        if (choice1.equals("q")){
+            System.exit(1);                                     //exits if instructed to do so by user
+        }
+        System.out.print("Enter Employee Number (1, 2, or 3): ");
+        Employee selection = employeeDB[stdin.nextInt()-1];     //selects the employee specified by the user from the previously created EmployeeDB Array
+        if (choice1.equals("p")){
+            try{ 
+                if (selection instanceof Associate){            //if the selected Employee is an Associate, then the user is prompted to enter hours worked
+                    System.out.print("Enter number of hours worked: ");
+                    System.out.println(selection +"\n" + String.format("$%.2f", selection.pay(stdin.nextInt())));
+                }
+            else System.out.println(String.format("$%.2f",selection.pay(0)));       //if the selected Employee is a manager, then the pay is calculated without additional prompting
             }
-        else System.out.println(String.format("$%.2f",selection.pay(0)));
+            catch (Exception e){
+                System.out.println("Not enough information is available to calculate pay for Employee "+ selection);       //if the type of employee -- Associate or Manager -- is not specified, an error is thrown
+            }
         }
-        catch (Exception e){
-            System.out.println("Not enough information is available to calculate pay for Employee "+ selection); 
+        else{
+            System.out.println(selection);                      //if the user originally selected "Employee Information", then the info for the selected employee is printed
         }
-    }
-    else{
-        System.out.println(selection);
-    }
     }
     
     }
@@ -43,35 +46,59 @@ abstract class Employee {
     public String toString() {
         return  this.FirstName + " " + this.LastName;
     }
-    public abstract double pay(int hours);
+    public abstract double pay(int hours);          //defines abstract pay method that will be implemented later
 
 }
 
 class Manager extends Employee{
-    private double yearSalary;
+    private double yearSalary;          //annual salary for the Manager
+
+    /**
+     * Constructor for the Manager Class which extends Employee
+     * @param fName
+     * @param lName
+     * @param salary
+     */
     public Manager(String fName, String lName, double salary) {
-        super(fName, lName);
-        yearSalary = salary;
+        super(fName, lName);        //passes name variables to superclass
+        yearSalary = salary;        //sets the salary according to user input
     }
 
+    /**
+     * Gets the previously stored annual salary
+     * @return double Salary
+     */
     public double getYearSalary() {
         return this.yearSalary;
     }
-
+    
     @Override
     public String toString() {
         return  super.toString() + ", Manager";
     }
+
+    /**
+     * implements abstract pay method, calculating bi-weekly salary calculated off of the annual salary stored previously
+     *  @param hours -- although there is an hours param for the sake of implementing the abstract pay method, it is unused in this implementation
+     *  @return double bi-weekly salary
+     */
     public double pay(int hours) {
-        return yearSalary/52*2;
+        return yearSalary/52*2;     
     }
 
 }
 
 class Associate extends Employee{
-    private double hourPayRate;
+    private double hourPayRate;         // hourly pay rate for the employee
+
+     /**
+     * Constructor for the Associate Class which extends Employee
+     * @param fName is the first name
+     * @param lName is the last name
+     * @param hourPayRate is the hourly pay rate for the employee, as entered by the user
+     */
     public Associate(String fName, String lName, double hourPayRate) {
-        super(fName, lName);
+        super(fName, lName);                //pasing name variables to the superclass
         this.hourPayRate = hourPayRate;
     }
 
@@ -83,6 +110,11 @@ class Associate extends Employee{
     public String toString() {
         return  super.toString() + ", Associate";
     }
+
+    /**
+     * implements abstract pay method, calculating the bi-weekly pay by taking hours worked <br/>and multiplying by the hourly rate
+     *@param hours is the hours worked
+     * */
     public double pay(int hours) {
         return hours * hourPayRate;
     }
--- Unit5/EvanKanter_A53_CarLot.java
+++ Unit5/EvanKanter_A53_CarLot.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class EvanKanter_A53_CarLot{
  public static void main (String[] args){
-    Vehicle[] inventory = {new Car(2019,"Toyota", "Corolla", "Red",1, 1, 2), new Minivan(2019,"Honda","Odyssey","White",1, 1, 4, 3), new Truck(2018, "Dodge", "Ram", "Grey",1, 2, 4, 8)};
+     //initalize an inventory of cars stored as an Array
+    Vehicle[] inventory = {new Car(2019,"Toyota", "Corolla", "Red",0, 1, 2), new Minivan(2019,"Honda","Odyssey","White",1, 0, 4, 3), new Truck(2018, "Dodge", "Ram", "Grey",1, 1, 4, 8)};
     
-    while (true){
+    while (true){           //begins looping, allowing the user to continously navigate through the menu until they select (Q)uit
         Scanner stdin = new Scanner (System.in);
-    System.out.print("Select one of the options: \n(V)ehicle Information\n(S)tatus\n(Q)uit\nEnter your choice: ");
-    String choice1 = stdin.next().toLowerCase();
+    System.out.print("Select one of the options: \n(V)ehicle Information\n(S)tatus\n(Q)uit\nEnter your choice: ");          // prints the main menu
+    String choice1 = stdin.next().toLowerCase();        //reads the user's selection, converting it to lower case
     if (choice1.equals("q")){
-        System.exit(1);
+        System.exit(1);                                 //exits if instructed to do so by user
     }
     System.out.print("Select vehicle:\n(C)ar\n(M)inivan\n(T)ruck\nEnter your choice: ");
     Vehicle selection;
-    switch(stdin.next().toLowerCase()){
+    switch(stdin.next().toLowerCase()){                 //uses a switch to select the vehicle specified by the user from the previously created inventory Array
         default:  
             selection = inventory[0];
             break;
@@ -25,13 +26,13 @@ public class EvanKanter_A53_CarLot{
             break;
     }
     if (choice1.equals("v")){
-        System.out.print("Vehicle Information:\n"+selection);
+        System.out.print("Vehicle Information:\n"+selection);       //if the user originally selected "Vehicle Information", then the info for the selected vehicle is printed
     }
     else{
-        System.out.print("Change Status:\n(1) On the lot \n(2) In Maintenance\n(3) Rented out\nEnter your choice: ");
-        selection.setVehicleStatus(stdin.nextInt()-1);
+        System.out.print("Change Status:\n(1) On the lot \n(2) In Maintenance\n(3) Rented out\nEnter your choice: ");      //if the user selected "Status", they are prompted to specify the new status for the vehicle
+        selection.setVehicleStatus(stdin.nextInt()-1);                  //the user-entered status is assigned as the new vehicle status for the selected vehicle
     }
-    System.out.println("\nThe vehicle is "+selection.getVehicleStatus()+".\n");
+    System.out.println("\nThe vehicle is "+selection.getVehicleStatus()+".\n");     //the vehicle status is printed to the console
 
     }
     
@@ -39,14 +40,22 @@ public class EvanKanter_A53_CarLot{
     }
 }
 
-abstract class Vehicle {
+class Vehicle {
     private int year;
     private String make;
     private String model;   
     private String colour;
     private int status;
-    private String[] statusCodes = {"on the lot", "in maintenance", "rented out"};
+    private String[] statusCodes = {"on the lot", "in maintenance", "rented out"};      //Array of status codes for cars in the dealership
 
+    /**
+     * constructor for generic vehicle class
+     * @param year
+     * @param make
+     * @param model
+     * @param colour
+     * @param status
+     */
     public Vehicle(int year, String make, String model, String colour, int status) {
         this.year = year;
         this.make = make;
@@ -54,11 +63,18 @@ abstract class Vehicle {
         this.colour = colour;
     }
     
+    /**
+     * gets the vehicle status as currently stored
+     * @return String vehicle status
+     */
     public String getVehicleStatus() {
         return statusCodes[this.status];
     }
 
-
+/**
+ * sets the new vehicle status to specified param value
+ * @param status
+ */
     public void setVehicleStatus(int status) {
         this.status = status;
     }
@@ -70,26 +86,38 @@ abstract class Vehicle {
     }
 }
 
+//defines a SpecificVehicle class extensing the generic Vehicle class
+//this is an appropriate technique as it prevents code duplication
 class SpecificVehicle extends Vehicle {
     private int fuelType;
     private int numDoors;
-    private String[] fuelTypes = {"Gas", "Diesel"};
+    private String[] fuelTypes = {"Gas", "Diesel"};         //Array storing the types of fuel for specific vehicles
 
+    /**
+     * Constructor for a specific vehicle
+     * takes 2 new params:
+     * @param fuelType
+     * @param numDoors
+     */
     public SpecificVehicle(int year, String make, String model, String colour, int status, int fuelType, int numDoors) {
-        super(year, make, model, colour, status);
-        this.fuelType = fuelType;
+        super(year, make, model, colour, status);       //passes properties of a generic vehicle to super class
+        this.fuelType = fuelType;                       
         this.numDoors = numDoors;
     }
 
     public String toString() {
-        return super.toString() + ", " + fuelTypes[fuelType] + ", " + numDoors + " doors";
+        return super.toString() + ", " + fuelTypes[fuelType] + ", " + numDoors + " doors";      //two string method converts the integer representation of fuel type into a String by pulling the String from the Array of Fuel Types
 
     }
 
 }
 class Car extends SpecificVehicle{
+
+    /**
+     * very simple constructor, simply passes values to the super class
+     */
     public Car(int year, String make, String model, String colour, int status, int fuelType, int numDoors) {
-        super(year, make, model, colour, status, fuelType, numDoors);
+        super(year, make, model, colour, status, fuelType, numDoors);       //passes params to superclass
     }
 
     public String toString() {
@@ -100,8 +128,12 @@ class Car extends SpecificVehicle{
 
 class Minivan extends SpecificVehicle{
     private int rowsSeats;
+    /**
+     * constructor for Minivan class, adds 1 new parameter in addition to those of the superclass
+     * @param rowsSeats
+     */
     public Minivan(int year, String make, String model, String colour, int status, int fuelType, int numDoors, int rowsSeats){
-        super(year, make, model, colour, status, fuelType, numDoors);
+        super(year, make, model, colour, status, fuelType, numDoors);       //passes params to superclass
         this.rowsSeats = rowsSeats;
     }
 
@@ -111,9 +143,14 @@ class Minivan extends SpecificVehicle{
 }
 
 class Truck extends SpecificVehicle{
-    private int engineCylinders;
+    private int engineCylinders;    //stores number of cylinders in the engine of the truck
+
+    /**
+     * constructor for Truck class, adds 1 new parameter in addition to those of the superclass
+     * @param engingeCylinders
+     */
     public Truck(int year, String make, String model, String colour, int status, int fuelType, int numDoors, int engingeCylinders){
-        super(year, make, model, colour, status, fuelType, numDoors);
+        super(year, make, model, colour, status, fuelType, numDoors);       //passes params to superclass
         this.engineCylinders = engingeCylinders;
     }
     public String toString() {
